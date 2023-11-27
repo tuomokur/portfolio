e5bc647ad6bd05fa1a3d0918080a7137a1f78d43
Tuomo Kurikka <tuomokur@gmail.com>
2023-11-27 11:19:39 +0200

    css and html clean up

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,13 @@ const About = () => {
   return (
     <>
       <div id='aboutAnchor' className='container-about'>
-        <img id='mypicture'src={me}/>
+        <img id='mypicture' alt='profilepicture' src={me}/>
         <div className="about-text-container">
           <h2 className='about-header'>About Me</h2>
-          <p className='about-text'>Hello! I am a Front End Developer with a keen interest in technology, data and business. I love to create intuitive and dynamic user experiences on the web. When I'm not coding, you can find me camping under the stars, at the gym or shredding my &#127928;</p>
+          <p className='about-text'>Hello! I am a Developer with a keen interest in technology, data and business. I love to create intuitive and dynamic user experiences on the web. When I'm not coding, you can find me camping under the stars, at the gym or shredding my &#127928;</p>
         </div>
       </div>
     </>
   )
 }
-
 export default About
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,18 +7,17 @@ const Nav = () => {
       <nav>
         <div className='nav'>
             <h1 id='pageTitle'>TuomoKurikka</h1>
-            <ul>
-              <a href='#aboutAnchor'><li>About</li></a>
-              <a href='#projectsAnchor'><li>Projects</li></a>
-              <a href='#experienceAnchor'><li>Experience</li></a>
-              <a href="https://github.com/tuomokur?tab=repositories"><li>GitHub</li></a>
-              <a href="https://www.linkedin.com/in/tuomo-kurikka/"><li>LinkedIn</li></a>
-              <a href={resume} target="_blank" id='resume'><li>CV &#128188; &#10138;</li></a>
+            <ul className='nav-ul'>
+              <a href='#aboutAnchor'><li className='nav-li'>About</li></a>
+              <a href='#projectsAnchor'><li className='nav-li'>Projects</li></a>
+              <a href='#experienceAnchor'><li className='nav-li'>Experience</li></a>
+              <a href="https://github.com/tuomokur?tab=repositories" target='_blank' rel='noopener noreferrer'><li className='nav-li'>GitHub</li></a>
+              <a href="https://www.linkedin.com/in/tuomo-kurikka/" target='_blank' rel='noopener noreferrer'><li className='nav-li'>LinkedIn</li></a>
+              <a href={resume} target='_blank' rel='noopener noreferrer'><li className='nav-li'>CV</li></a>
             </ul>
         </div>
       </nav>
     </>
   )
 }
-
 export default Nav
